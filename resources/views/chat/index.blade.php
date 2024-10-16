7391daf63bdab4d5e155c0791897a9d0ec768874
“yuki” <yuki12240221@au.com>
2024-10-17 03:43:25 +0900

    block

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -7,20 +7,35 @@
 
     <div class="py-4">
         @foreach($users as $user)
-            @if ($user->id !== auth()->id())  <!-- ログインユーザーをスキップ -->
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-4">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <a href="{{ route('chat.show', ['user' => $user->id]) }}" class="text-gray-800 dark:text-gray-300 text-lg hover:underline">
-                            {{ $user->name }}
-                        </a>
-                        <div class="text-gray-600 dark:text-gray-400 text-sm">
-                            <p>アカウント作成日時: {{ $user->created_at->format('Y-m-d H:i') }}</p>
-                        </div>
+        @if ($user->id !== auth()->id()) <!-- ログインユーザーをスキップ -->
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-4">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <a href="{{ route('chat.show', ['user' => $user->id]) }}" class="text-gray-800 dark:text-gray-300 text-lg hover:underline">
+                        {{ $user->name }}
+                    </a>
+                    <div class="text-gray-600 dark:text-gray-400 text-sm">
+                        <p>アカウント作成日時: {{ $user->created_at->format('Y-m-d H:i') }}</p>
+                    </div>
+                    <!--ブロック-->
+                    <div class="flex">
+                        @if ($user->blocked->contains(auth()->id()))
+                        <form action="{{ route('block.destroy', $user) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="text-red-500 hover:text-red-700">ブロックを解除する</button>
+                        </form>
+                        @else
+                        <form action="{{ route('block.store', $user) }}" method="POST">
+                            @csrf
+                            <button type="submit" class="text-blue-500 hover:text-blue-700">ブロックする</button>
+                        </form>
+                        @endif
                     </div>
                 </div>
             </div>
-            @endif
+        </div>
+        @endif
         @endforeach
     </div>
 </x-app-layout>
